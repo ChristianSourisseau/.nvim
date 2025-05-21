3b5fe15ca47306ff7980954c149ad9a562d5ba82
Christian Sourisseau <christianf.sourisseau@gmail.com>
2025-05-21 09:42:18 -0700

    new plugin edit original file when opening a symlink

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -41,9 +41,11 @@ require('lazy').setup({
 
   require 'plugins/mini',
 
+  require 'plugins/vim-symlink',
+
   require 'plugins/treesitter',
-  
- require 'plugins.colorful-winsep',
+
+  require 'plugins.colorful-winsep',
   -- The following comments only work if you have downloaded the repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
